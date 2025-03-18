6240c7f55879bf4886424d110e4bc28435cf7014
architajain2003 <59245825+architajain2003@users.noreply.github.com>
2025-03-18 10:02:32 +0530

    nav changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,10 +9,12 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
+      {/* Logo */}
       <Link to="/">
         <img src={assets.logo} alt="logo" className="w-36" />
       </Link>
 
+      {/* Navigation Links (for larger screens) */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
@@ -32,39 +34,41 @@ const Navbar = () => {
         </NavLink>
       </ul>
 
+      {/* Right Icons (Search, Profile, Cart, Menu for small screens) */}
       <div className="flex items-center gap-6">
+        {/* Search Icon */}
         <img
-          onClick={() => {
-            setShowSearch(true);
-          }}
+          onClick={() => setShowSearch(true)}
           src={assets.search_icon}
-          alt=""
-          className="w-5 cursor-pointer "
+          alt="search"
+          className="w-5 cursor-pointer"
         />
+
+        {/* Profile Icon */}
         <div className="group relative">
           <Link to="/login">
             <img
               src={assets.profile_icon}
-              alt=""
+              alt="profile"
               className="w-5 cursor-pointer"
             />
           </Link>
-          
-          </div>
         </div>
 
-        <Link to="/cart" className="relative ">
-          <img src={assets.cart_icon} alt="" className="w-5 min-w-5 " />
+        {/* Cart Icon with Cart Count */}
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="cart" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             {getCartCount()}
           </p>
         </Link>
 
+        {/* Menu Icon for Small Screens */}
         <img
           src={assets.menu_icon}
-          alt=""
-          className="w-5 cursor-pointer sm:hidden "
-          onClick={() => setVisible(!visible)} //visibility toggle (true/false))
+          alt="menu"
+          className="w-5 cursor-pointer sm:hidden"
+          onClick={() => setVisible(!visible)} // Toggle sidebar visibility
         />
       </div>
 
@@ -73,56 +77,43 @@ const Navbar = () => {
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white ease-in duration-300
              ${visible ? 'w-full' : 'w-0'}`}
       >
-        <div className="flex flex-col text-gray-600 ">
+        <div className="flex flex-col text-gray-600">
           <div
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="flex items-center gap-4 p-3 cursor-pointer"
           >
-            <img src={assets.dropdown_icon} alt="" className="h-4 rotate-180" />
+            <img src={assets.dropdown_icon} alt="dropdown" className="h-4 rotate-180" />
             <p className="font-semibold">Back</p>
           </div>
 
+          {/* Sidebar Navigation Links */}
           <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="py-2 pl-6 border"
             to="/"
           >
-            {' '}
-            HOME{' '}
+            HOME
           </NavLink>
           <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="py-2 pl-6 border"
             to="/collection"
           >
-            {' '}
-            COLLECTION{' '}
+            COLLECTION
           </NavLink>
           <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="py-2 pl-6 border"
             to="/about"
           >
-            {' '}
-            ABOUT{' '}
+            ABOUT
           </NavLink>
           <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="py-2 pl-6 border"
             to="/contact"
           >
-            {' '}
-            CONTACT{' '}
+            CONTACT
           </NavLink>
         </div>
       </div>
